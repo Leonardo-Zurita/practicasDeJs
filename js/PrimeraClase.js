20920996bce03e40c8e6288742206a9a2ebe853b
Leonardo Zurita <leonardodanielzurita@gmail.com>
2022-09-15 19:01:14 -0300

    primer commit

---
+++ js/PrimeraClase.js
@@ -0,0 +1,29 @@
+console.log("Hola mundo");
+// document.write("Hola mundo"); Esto sirve para que lo escrito aparesca en la pagina
+
+console.log("Este est un 2do mensaje");
+
+const PI = 3.14;
+console.log(typeof PI)
+
+// prompt abre una ventana en la pagina y esta funcion es del tipo entrada y salida
+let suma = 0
+let ValorDeEntrada = Number(prompt("Indique un Valor"));
+console.log(ValorDeEntrada);
+ 
+suma = suma + ValorDeEntrada;
+ValorDeEntrada = Number(prompt("Indique un Valor"));
+console.log(ValorDeEntrada);
+ 
+suma = suma + ValorDeEntrada;
+ValorDeEntrada = Number(prompt("Indique un Valor"));
+console.log(ValorDeEntrada);
+ 
+suma = suma + ValorDeEntrada;
+ValorDeEntrada = Number(prompt("Indique un Valor"));
+console.log(ValorDeEntrada);
+
+suma = suma + ValorDeEntrada;
+
+console.log("La suma es de " + suma)
+ 
